bee3c254e66323068d539e74fabc559e81e31c62
Logan McAnsh <logan@mcan.sh>
2017-12-18 19:18:17 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,13 @@
-// Code Invitation Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Invitation = ({ children }) => (
+  <div>
+    <h1>You've been invited!</h1>
+    {children}
+  </div>
+);
+
+Invitation.propTypes = {};
+
+export default Invitation;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'raf/polyfill';
 
 import Invitation from './components/Invitation';
 import ThemedParty from './components/ThemedDecorations';
 
 ReactDOM.render(
   <div>
-    <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
+    <Invitation>
+      Hey, you should totally come to this awesome party I'm throwing!
+    </Invitation>
     <ThemedParty theme="heaven">
       <p>A decoration</p>
       <p>Some other decoration</p>
       <p>A last decoration</p>
     </ThemedParty>
   </div>,
-  document.getElementById('global')
+  document.getElementById('global'),
 );
